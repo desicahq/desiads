41eba6a73a9b099f1bc0e31866d9528a5f76a03c
Jacob Marshall <jacob.marshall.jem@gmail.com>
2021-03-09 13:42:10 +0000

    Fix mobile account margin

--- pages/account.js
+++ pages/account.js
@@ -53,7 +53,7 @@ export default function Account() {
   return (
     <section className="bg-black mb-32">
       <div className="max-w-6xl mx-auto pt-8 sm:pt-24 pb-8 px-4 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:flex-col sm:align-center">
+        <div className="sm:flex sm:flex-col sm:align-center mt-16">
           <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
             Account
           </h1>
