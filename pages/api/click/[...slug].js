da91573a0da9bfe3db23d14880cd2a5e6af0abbc
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-06-03 17:43:13 +0100

    🥅 Better error catching

--- pages/api/click/[...slug].js
+++ pages/api/click/[...slug].js
@@ -3,24 +3,24 @@ import Cors from 'cors'
 
 // Initializing the cors middleware
 const cors = Cors({
-  methods: ['GET', 'HEAD'],
+    methods: ['GET', 'HEAD'],
 })
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
 function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
 
-      return resolve(result)
+            return resolve(result)
+        })
     })
-  })
 }
 
-const click = async (req, res) => {  
+const click = async (req, res) => {
     await runMiddleware(req, res, cors)
 
     const { slug } = req.query
@@ -30,13 +30,15 @@ const click = async (req, res) => {
         .select('*')
         .eq('id', slug[0])
 
-        let { data: site, siteErr } = await supabase
+    let { data: site, siteErr } = await supabase
         .from('sites')
         .select('*')
         .eq('id', slug[1])
 
-    if (!slug[0]) return res.status(404).end(`Desiad campaign not found`)
-    if (!slug[1]) return res.status(403).end(`You'll need to specify your domain id`)
+    if (!slug[0]) return res.status(404).end(`No campaign slug was provided`)
+    if (!slug[1]) return res.status(403).end(`You'll need to specify your site id`)
+    if (!ad[0]) return res.status(404).end(`Desiad campaign not found`)
+    if (!site[0]) return res.status(404).end(`Your desiad site id: ${slug[1]} is invalid or missing`)
 
     let adClickCount = ad[0].clicks + 1
     let siteClickCount = site[0].clicks + 1
@@ -44,18 +46,18 @@ const click = async (req, res) => {
 
     let { data: updateAdClicks, updateAdErr } = await supabase
         .from('ads')
-        .update({clicks: adClickCount})
+        .update({ clicks: adClickCount })
         .eq('id', slug[0])
 
     let { data: updateSiteClicks, updateSiteErr } = await supabase
         .from('sites')
-        .update({clicks: siteClickCount})
+        .update({ clicks: siteClickCount })
         .eq('id', slug[1])
 
     console.log(slug[0])
 
     return res.redirect(ad[0].url)
 
-  }
-  
-  export default click
+}
+
+export default click
