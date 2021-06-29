8a4a1e364a17a9563e118c522ffcbf15e1973fa8
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-06-29 09:32:16 +0100

    📈 Use analytics

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head />
         <body className="loading">
           <Main />
+          <script async defer data-website-id="798fa9ef-e71f-4a9e-8dcd-c7a417ce19c9" src="https://analytics.desica.uk/umami.js"></script>
           <NextScript />
         </body>
       </Html>
